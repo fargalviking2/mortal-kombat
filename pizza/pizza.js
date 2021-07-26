cb46f6a22679a0caff258ec843b8abec31d779a0
fargalviking2 <80988313+fargalviking2@users.noreply.github.com>
2021-07-26 23:40:56 +0300

    Add files via upload

--- pizza/pizza.js
+++ pizza/pizza.js
@@ -16,23 +16,65 @@ const App = {
 			chickenBlueCost: "349",
 			asiaChickenCost: "279",
 			italianCost: "299",
-			counter: 0,
-			counter2: 0,
-			counter3: 0,
-			counter4: 0,
+
+			counterLosos: 0,
+			counterLosos25: 0,
+			counterLosos30: 0,
+			counterLosos35: 0,
+			counterBlue25: 0,
+			counterBlue30: 0,
+			counterBlue35: 0,
+			counterAsian25: 0,
+			counterAsian30: 0,
+			counterAsian35: 0,
+			counterItalian25: 0,
+			counterItalian30: 0,
+			counterItalian35: 0,
+
 			infoCounter: 1,
 			infoCounter2: 1,
 			infoCounter3: 1,
 			infoCounter4: 1,
+
+			types: [
+				{ id: 1, title: 'hervam' },
+				{ id: 2, title: 'hervam2' },
+			],
 		}
 	},
-	methods: {
+	// methods: {
+	// 	counterLososPlus() {
+	// 		this.counterLosos++;
+	// 	}
+	// },
+	// computed: {
 
-	},
-	computed: {
-
-
-	},
+	// },
 }
-
-Vue.createApp(App).mount('#app')
+// Vue.createApp(App).mount('#app')
+const app = Vue.createApp(App)
+app.component('type-block', {
+	data() {
+		return {
+			count: 0
+		}
+	},
+	props: ['actualCost', 'needCost',],
+	template: `
+	<template v-if="actualCost == needCost">
+	<button v-if="count < 1" @click="count++"
+		class="grid-template-pizza__to-bascet-button">В
+		корзину</button>
+	<div v-else class="grid-template-pizza__in-bascet">
+		<div class="grid-template-pizza__gal">
+			<img src="img/gal.svg" alt="">
+		</div>
+		<button @click="count--" type="submit" class="counter--">-</button>
+		<div class="counter">{{count}}</div>
+		<button @click="count++" type="submit" class="counterplus">+</button>
+	</div>
+</template>
+`
+})
+// app.mount('#type-blocks')
+app.mount('#app')
